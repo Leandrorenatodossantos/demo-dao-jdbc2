8a6f5a9d9a5c25e7e7c6c68bedd105aa24e15040
Leandrorenatodossantos <leandrosantos.contabilidade@gmail.com>
2020-06-22 07:31:06 -0300

    seller add class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(1, "Rafael", "teste@teste", new Date(), 1900.00, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
